e9988ebaa63303063d42a3bd4ac460186f3bec08
Radin Movahedi <radinmov@Radins-MacBook-Air.local>
2024-10-24 10:28:37 +0330

    public_folder update , title

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,4 +1,7 @@
+import useTitle from "../../Componets/Hook/useTitle"
+
 export const Home = () => {
+    useTitle("HomePage")
     return (
         <p className="bg-sky-600">welcome to home page </p>
     )
--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,4 +1,7 @@
+import useTitle from "../../Componets/Hook/useTitle"
+
 export const Login = () => {
+    useTitle("Login")
     return (
         <p className="font-bold">Login page </p>
     )
