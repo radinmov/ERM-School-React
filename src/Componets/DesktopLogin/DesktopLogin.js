beab32310669c883e2e6cfbce3176a2b560a057b
sana <mojsagh.83@gmail.com>
2024-11-02 13:33:27 +0330

    desktop absolate

--- src/Componets/DesktopLogin/DesktopLogin.js
+++ src/Componets/DesktopLogin/DesktopLogin.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import LoginForm from '../LoginForm/LoginForm'
-import WellcomeSquare from '../WellcomeSquare/WellcomeSquare'
+import React from 'react';
+import LoginForm from '../LoginForm/LoginForm';
+import WellcomeSquare from '../WellcomeSquare/WellcomeSquare';
 
 const DesktopLogin = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">    
-      <div className="relative w-1/2">
+      <div className="relative w-4/5 max-w-5xl flex transform -right-1/4 -translate-x-1/2 h-5/6   items-center">
+        {/* بخش خوش‌آمد */}
         <WellcomeSquare />
-        <div className="bg-white absolute top-5 left-5 w-4/5 h-2/3 p-5 border-2 border-black rounded-xl flex items-center justify-center">
-          <LoginForm />      
+      </div> 
+        {/* بخش فرم ورود */}
+        <div className="absolute top-1/2 max-w-5xl transform -translate-x-1/2 -translate-y-1/2 w-[400px] h-auto p-8 border border-gray-200 rounded-xl shadow-lg bg-white z-10">
+          <LoginForm />
         </div>
-      </div>         
+              
     </div>
-  )
+  );
 }
 
-export default DesktopLogin
+export default DesktopLogin;
+
+
